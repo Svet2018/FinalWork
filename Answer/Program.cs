dc921763d4632f6a91066a8e5e6b58a6cb028891
Svetlana <bulatova44@gmail.com>
2022-09-13 19:46:15 +0300

    Added GenerateArray method

--- Answer/Program.cs
+++ Answer/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая изимеющегося массива строк формирует 
+//массив из строк длина которых меньше либо равна три символа. Первоначальный массив
+//можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении 
+//не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+//
+
+Console.Clear();
+
+void GenerateArray(string [] array, int arrayLength)
+{
+    int sum = 0;
+    for(int i = 0; i < arrayLength; i++)
+    {
+        sum++;
+        System.Console.WriteLine($"Введите {sum} строку из {arrayLength}:");
+        array[i] = Console.ReadLine();     
+    }       
+}
